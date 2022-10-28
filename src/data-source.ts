a049c0c8dd10a317835928df86eb6a418c12b5c6
Francisco Stenico <francisco.stenico@gmail.com>
2022-10-28 11:58:51 -0300

    CHORE(Heroku): Versionamento da API

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,6 +1,6 @@
 import { DataSource } from 'typeorm';
 import 'dotenv/config';
-
+// prettier-ignore
 const AppDataSource = new DataSource(
   process.env.NODE_ENV === 'test'
     ? {
@@ -11,15 +11,12 @@ const AppDataSource = new DataSource(
       }
     : {
         type: 'postgres',
-        host: process.env.HOST,
-        port: 5432,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
+        url: process.env.DATABASE_URL,
+        ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
         logging: true,
         synchronize: false,
-        entities: ['src/entities/*.ts'],
-        migrations: ['src/migrations/*.ts'],
+        entities: process.env.NODE_ENV ? ['dist/src/entities/*.js'] : ['src/entities/*.ts'],
+        migrations: process.env.NODE_ENV ? ['dist/src/migrations/*.js'] : ['src/migrations/*.ts'],
       }
 );
 
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import app from './app';
 import AppDataSource from './data-source';
+import 'dotenv/config';
 
+const PORT = process.env.PORT || 3000;
 (async () => {
   await AppDataSource.initialize().catch((err) => {
     console.error('Error during Data Source initialization', err);
   });
 
-  app.listen(3000, () => {
-    console.log('Server running on port 3000');
+  app.listen(PORT, () => {
+    console.log(`Connected to the server.`, { PORT });
   });
 })();
